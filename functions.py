abbc8a2c6c39fe64b1d917bde3a40b673e85a365
osbaldoAlbornoz <46227344+osbaldoAlbornoz@users.noreply.github.com>
2018-12-28 12:28:31 -0600

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,48 @@
+# Funciones V5
+
+#ejemplo de funcion
+
+# def Mensaje():
+# 	print("Aprendiendo Python")
+# 	print("Aprendiendo lo basico")
+# 	print("Aprendiendo poco a poco")
+# finaliza la funcion aqui, no se especifica donde finaliza solo salir de iden
+
+#Mensaje()
+
+#-------------------------------------------------------------
+
+
+
+#Funciones con parametros V6
+#sin return
+
+# def suma(num1, num2):
+
+# 	print(num1+num2)
+# #
+# suma(5,7)
+
+
+#Funciones con parametros V6
+#con return
+def suma2(num1, num2):
+
+	suma=num1+num2
+	return suma
+#
+print(suma2(5,7))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
